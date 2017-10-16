619bd50fe878aba189fb629b2f7e93d335b34c1f
tobiasosir <32336675+tobiasosir@users.noreply.github.com>
2017-10-16 15:17:25 -0600

    Update STREG.py

--- STREG.py
+++ STREG.py
@@ -10,3 +10,16 @@ Trek = ['TOS', 'TNG', 'DS9', 'VOY', 'ENT']
 print (random.choice(Trek))
 #This will select a series, then the season (1-7) then the episode (1-24)
 print (random.choice(Trek), y, z)
+#this will assign the above to a variable that can be easily used in the rest of the code
+var = (random.choice(Trek))
+#however, this will establish var as the same choice every time. The variable needs to be deleted each time the script is run:
+del var
+var = (random.choice(Trek))
+if var == TOS:
+  y = (randint(1,3))
+  print (var, y)
+else:
+  y = (randint(1,7))
+  print (var, y)
+del var
+del y
